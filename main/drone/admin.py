858cc0b1c4125b79ee2f5c81417cf8f1d1a42740
Sunny <soodsunny80@gmail.com>
2022-04-15 12:12:06 -0500

    Removed old django code

--- main/drone/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import RegisteredUsers, UnRegisteredUsers, tickets
-# Register your models here.
-admin.site.register(RegisteredUsers)
-admin.site.register(UnRegisteredUsers)
-admin.site.register(tickets)
-
-
